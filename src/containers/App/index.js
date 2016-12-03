9b06caf4f572b74c580ef44ff4d5da28cf79ceb9
aah13002 <aaron.huang@uconn.edu>
2016-12-03 11:40:39 -0500

    Leaflet on App

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,22 +1,14 @@
-import React, { PropTypes } from 'react';
-import Helmet from 'react-helmet';
-import config from '../../config';
+import React from 'react';
+import LeafletMap from '../LeafletMap';
 
 import '../../theme/normalize.css';
 import styles from './styles.scss';
 
-const App = ({ children }) => (
+const App = () => (
   <div className={styles.App}>
-    <Helmet {...config.app} />
-    <div className={styles.header}>
-      <img src={require('./assets/logo.svg')} alt="Logo" role="presentation" />
-      <h1>{config.app.title}</h1>
-    </div>
-    <hr />
-    {children}
+    <LeafletMap />
   </div>
 );
 
-App.propTypes = { children: PropTypes.node };
 
 export default App;
