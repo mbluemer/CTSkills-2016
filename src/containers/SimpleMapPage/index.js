02cf27c842b64cc66b0df15b623f097ed13989cf
aah13002 <aaron.huang@uconn.edu>
2016-12-03 12:36:23 -0500

    SimpleMapPage works now

--- src/containers/SimpleMapPage/index.js
+++ src/containers/SimpleMapPage/index.js
@@ -4,6 +4,8 @@ import shouldPureComponentUpdate from 'react-pure-render/function';
 import GoogleMap from 'google-map-react';
 import MyGreatPlace from './my_great_place';
 
+import styles from './styles.scss';
+
 export default class SimpleMapPage extends Component {
   static defaultProps = {
     center: { lat: 59.938043, lng: 30.337157 },
@@ -20,20 +22,22 @@ export default class SimpleMapPage extends Component {
 
   render() {
     return (
-      <GoogleMap
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-      >
-        <MyGreatPlace lat={59.955413} lng={30.337844} text={'A'} /* Kreyser Avrora */ />
-        <MyGreatPlace {...this.props.greatPlaceCoords} text={'B'} /* road circle */ />
-      </GoogleMap>
+      <div className={styles.SimpleMapPage}>
+        <GoogleMap
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <MyGreatPlace lat={59.955413} lng={30.337844} text={'A'} /* Kreyser Avrora */ />
+          <MyGreatPlace {...this.props.greatPlaceCoords} text={'B'} /* road circle */ />
+        </GoogleMap>
+      </div>
     );
   }
 }
 
 /* eslint-disable */
 SimpleMapPage.propTypes = {
-  center: PropTypes.number,
+  center: PropTypes.object,
   zoom: PropTypes.number,
   greatPlaceCoords: PropTypes.object,
 };
