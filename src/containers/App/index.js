1d596ddc8d12620a3f3fccd04c032235c4b596e5
John Bojorquez <john.bojorquez@uconn.edu>
2016-12-03 11:47:39 -0500

    Add SearchBar component

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,9 +2,8 @@ import React, { PropTypes } from 'react';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import Paper from 'material-ui/Paper';
-import TextField from 'material-ui/TextField';
 import classNames from 'classnames';
+import SearchBar from './SearchBar';
 
 import '../../theme/normalize.css';
 import styles from './styles.scss';
@@ -12,11 +11,7 @@ import styles from './styles.scss';
 const App = () => (
   <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
     <div className={classNames(styles.App, 'container')}>
-      <Paper className={styles.Search} zDepth={2} rounded={false}>
-        <div className={styles.SearchField}>
-          <TextField hintText="Full width" fullWidth />
-        </div>
-      </Paper>
+      <SearchBar />
     </div>
   </MuiThemeProvider>
 );
